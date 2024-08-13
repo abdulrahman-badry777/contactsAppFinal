5a35ed02ba48a576e78e5ef87f4396cfb57981ee
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 19:42:41 +0300

    Update view.py

--- contactsAppFinal/routes/view.py
+++ contactsAppFinal/routes/view.py
@@ -4,6 +4,4 @@ view_bp = Blueprint('view', __name__)
 
 @view_bp.route("/View")
 def View():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
     return render_template("view_contact.html", custom_css="view.css")
