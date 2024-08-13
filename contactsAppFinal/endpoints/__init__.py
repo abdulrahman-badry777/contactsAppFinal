84dc13ba0f053d2ea46980b3edb8810ad7628329
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 15:49:34 +0300

    Update __init__.py
    routes blueprints done

--- contactsAppFinal/endpoints/__init__.py
+++ contactsAppFinal/endpoints/__init__.py
@@ -4,11 +4,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
-    app.register_blueprint(loginBp)
-    app.register_blueprint(contactsBp)
-    app.register_blueprint(contactRemoveBp)
-    app.register_blueprint(add_contactsBp)
-    app.register_blueprint(contact_detailsBp)
+    app.register_blueprint(login_renderBp)
+    app.register_blueprint(contactListBp)
+    app.register_blueprint(addContactBp)
+    app.register_blueprint(ViewBp)
+    app.register_blueprint(edit_contactBp)
+    
     app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 
     return app
