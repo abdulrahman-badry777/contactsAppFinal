f32bebaeeefbe0c66f3165c0c5855f491c5e9e2c
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 19:35:14 +0300

    Update edit_contact.py

--- contactsAppFinal/routes/edit_contact.py
+++ contactsAppFinal/routes/edit_contact.py
@@ -4,8 +4,12 @@ edit_contact_bp = Blueprint('edit_contact', __name__)
 
 @edit_contact_bp.route("/edit_contact")
 def edit_contact_page():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     return render_template("edit_contact.html", custom_css="edit_contact.css")
 
 @edit_contact_bp.route("/Update_Contact")
 def Update_Contact():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     return render_template("Update_Contact.html", custom_css="Update_Contact.css")
