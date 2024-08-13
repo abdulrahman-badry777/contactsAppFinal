074c363191b5561ea3861ad3f2738f72b33ea125
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 15:52:46 +0300

    Update app.py

--- contactsAppFinal/app.py
+++ contactsAppFinal/app.py
@@ -1,7 +1,4 @@
 
-from flask import Flask, Flask, render_template, request, redirect, url_for, session, jsonify
-
-from utils.establishDBConnection import get_db_connection
 
 app = Flask(__name__, template_folder='templates')
 
