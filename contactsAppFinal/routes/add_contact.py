0de4850b14fc3aabd8e30e37c0b62d09490a2640
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 19:35:02 +0300

    Update add_contact.py

--- contactsAppFinal/routes/add_contact.py
+++ contactsAppFinal/routes/add_contact.py
@@ -4,4 +4,6 @@ add_contact_bp = Blueprint('add_contact', __name__)
 
 @add_contact_bp.route('/addContact')
 def addContact():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     return render_template("add_contact.html", custom_css="add_contact.css")
