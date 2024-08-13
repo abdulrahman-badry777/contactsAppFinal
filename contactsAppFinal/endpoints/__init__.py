f4e1ff9dce8684fe365fe6c6d111e43ae1d912b3
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 16:01:34 +0300

    Update __init__.py

--- contactsAppFinal/endpoints/__init__.py
+++ contactsAppFinal/endpoints/__init__.py
@@ -1,4 +1,5 @@
 from .routes import addContactBp,login_renderBp,contactListBp,ViewBp,edit_contactBp
+from .crud import   loginBp,contactsBp,viewContactBp,editcontactBp,DeleteContactBp,add_contactBp
 from flask import Flask
 
 
@@ -9,7 +10,12 @@ def create_app():
     app.register_blueprint(addContactBp)
     app.register_blueprint(ViewBp)
     app.register_blueprint(edit_contactBp)
-    
+    app.register_blueprint(loginBp)
+    app.register_blueprint(contactsBp)
+    app.register_blueprint(viewContactBp)
+    app.register_blueprint(editcontactBp)
+    app.register_blueprint(DeleteContactBp)
+    app.register_blueprint(add_contactBp)
     app.config['EXPLAIN_TEMPLATE_LOADING'] = True
 
     return app
