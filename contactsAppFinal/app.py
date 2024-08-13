05a99d1c6e3c99d54de4a20044e272d7c5ecfe9c
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-13 16:02:52 +0300

    Update app.py

--- contactsAppFinal/app.py
+++ contactsAppFinal/app.py
@@ -1,10 +1,6 @@
+from endpoints import create_app
 
-
-app = Flask(__name__, template_folder='templates')
-
-#The login page (Ahmed Naga)
-
- 
+app = create_app() 
 
 if __name__ == "__main__":
     app.run(debug=True)
