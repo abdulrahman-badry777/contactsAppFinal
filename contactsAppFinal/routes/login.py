ef9c8e42e582dbc160397a3b4a6305a9473bf2d4
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-14 11:56:27 +0300

    Update login.py

--- contactsAppFinal/routes/login.py
+++ contactsAppFinal/routes/login.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify
+
 from contactsAppFinal.utils.establishDBConnection import get_db_connection
 
 login_bp = Blueprint('login', __name__)
@@ -11,8 +12,12 @@ def login():
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("SELECT id FROM users WHERE email=? AND password=?", (email, password))
-        user = cur.fetchall()
+        user = cur.fetchone()
         cur.close()
-        return jsonify(data=user) 
+        conn.close()
+        if user:
+            return jsonify({'status': 'success', 'user_id': user[0]})
+        else:
+            return jsonify({'status': 'error', 'message': 'Invalid email or password'})
     else:
         return render_template("login.html", custom_css="login.css")
