3704c5bb237a00603c2b39c9ae5cf853db3ac41c
Ahmed haytham <95971046+AhmedHaythamNaga@users.noreply.github.com>
2024-08-14 11:57:00 +0300

    Update login.js

--- contactsAppFinal/static/js/login.js
+++ contactsAppFinal/static/js/login.js
@@ -1,55 +1,54 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();  
+    event.preventDefault();
 
     const email = document.getElementById('mail').value;
     const password = document.getElementById('pass').value;
 
-fetch('/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: new URLSearchParams({
-        email: email,
-        password: password
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            email: email,
+            password: password
+        })
     })
-})
-.then(response => {
-    if(response.status === 200) {
-        return response.json();
-    } else {
-        throw new Error('Failed to fetch users.');
-    }
-})
-.then(users => {
-    console.log(users.data);
-    let user = false;
-    for (let i = 0; i < users.data.length; i++) {
-        if(users.data[i][1] === email && users.data[i][2] === password) {
-            user = true;
-            window.localStorage.setItem("user_id", `${users.data[i][0]}`);
-            break;
-    }
-}
-    if (user) {
-        Swal.fire({
-            title: 'Success!',
-            text: 'You have logged in successfully.🥳',
-            icon: 'success',
-            confirmButtonText: 'OK'
-        }).then(() => {
-            window.location.pathname = `/contactList`;
-        });
-    } else {
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Failed to fetch users.');
+        }
+    })
+    .then(data => {
+        if (data.status === 'success') {
+            window.localStorage.setItem("user_id", data.user_id);
+            Swal.fire({
+                title: 'Success!',
+                text: 'You have logged in successfully.🥳',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                window.location.pathname = `/contactList`;
+            });
+        } else {
+            Swal.fire({
+                title: 'Error!',
+                text: data.message,
+                icon: 'error',
+                confirmButtonText: 'Try Again'
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching user data:', error);
         Swal.fire({
             title: 'Error!',
-            text: 'Invalid email or password.🙄',
+            text: 'An unexpected error occurred.',
             icon: 'error',
             confirmButtonText: 'Try Again'
         });
-    }
-})
-.catch(error => {
-    console.error('Error fetching user data:', error);
+    });
 });
-});
+
